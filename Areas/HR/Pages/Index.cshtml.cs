5b3fc38fd987ac2b6760eb8e0500c583ae1aaaf7
Dev-triggz <arotroy01@hotmail.com>
2025-03-26 22:28:07 -0400

    yes

---
+++ Areas/HR/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor_pages.HR
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
